3a07938b5c3ec4e0dc570b1f6d03759a22a70c49
scotteggs <scotteggs@gmail.com>
2016-05-14 15:48:19 -0400

    creates Note and Notes components and enables editing

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,3 +1,80 @@
 import React from 'react';
 
-export default () => <div>Mercury Loves Transition</div>;
+export default class Note extends React.Component {
+  constructor(props) {
+    super(props);
+
+    // Track `editing` state.
+    this.state = {
+      editing: false
+    };
+  }
+  render() {
+    // Render the component differently based on state.
+    if(this.state.editing) {
+      return this.renderEdit();
+    }
+
+    return this.renderNote();
+  }
+  renderEdit = () => {
+    // We deal with blur and input handlers here. These map to DOM events.
+    // We also set selection to input end using a callback at a ref.
+    // It gets triggered after the component is mounted.
+    //
+    // We could also use a string reference (i.e., `ref="input") and
+    // then refer to the element in question later in the code through
+    // `this.refs.input`. We could get the value of the input using
+    // `this.refs.input.value` through DOM in this case.
+    //
+    // Refs allow us to access the underlying DOM structure. They
+    // can be using when you need to move beyond pure React. They
+    // also tie your implementation to the browser, though.
+    return <input type="text"
+      ref={
+        element => element ?
+        element.selectionStart = this.props.task.length :
+        null
+      }
+      autoFocus={true}
+      defaultValue={this.props.task}
+      onBlur={this.finishEdit}
+      onKeyPress={this.checkEnter} />;
+  };
+  renderNote = () => {
+    // If the user clicks a normal note, trigger editing logic.
+    return <div onClick={this.edit}>{this.props.task}</div>;
+  };
+  edit = () => {
+    // Enter edit mode.
+    this.setState({
+      editing: true
+    });
+  };
+  checkEnter = (e) => {
+    // The user hit *enter*, let's finish up.
+    if(e.key === 'Enter') {
+      this.finishEdit(e);
+    }
+  };
+  finishEdit = (e) => {
+    // `Note` will trigger an optional `onEdit` callback once it
+    // has a new value. We will use this to communicate the change to
+    // `App`.
+    //
+    // A smarter way to deal with the default value would be to set
+    // it through `defaultProps`.
+    //
+    // See the *Typing with React* chapter for more information.
+    const value = e.target.value;
+
+    if(this.props.onEdit) {
+      this.props.onEdit(value);
+
+      // Exit edit mode.
+      this.setState({
+        editing: false
+      });
+    }
+  };
+}
