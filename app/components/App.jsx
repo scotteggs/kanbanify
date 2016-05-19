9035913c78d1e63f72c7e1eaaafb5eefff991ed9
scotteggs <scotteggs@gmail.com>
2016-05-19 17:11:29 -0400

    implemnts existing CRUD utilizing Alt FLUX implementation

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,29 +1,27 @@
-import uuid from 'node-uuid';
 import React from 'react';
 import Notes from './Notes.jsx';
+
+import NoteActions from '../actions/NoteActions.js';
+import NoteStore from '../stores/NoteStore';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
+    this.state = NoteStore.getState();
   }
-
+  componentDidMount() {
+    NoteStore.listen(this.storeChanged);
+  }
+  componentWillUnmount() {
+    NoteStore.unlisten(this.storeChanged);
+  }
+  storeChanged = (state) => {
+    // Without a property initializer `this` wouldn't
+    // point at the right context because it defaults to
+    // `undefined` in strict mode.
+    this.setState(state);
+  };
   render() {
   	const notes = this.state.notes;
     return (
@@ -35,36 +33,26 @@ export default class App extends React.Component {
       </div>
     );
   }
-  addNote = () => {
-  	this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
-  };
-	deleteNote = (id, e) => {
+
+  deleteNote(id, e) {
     // Avoid bubbling to edit
     e.stopPropagation();
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
-  };
-  editNote = (id, task) => {
+    NoteActions.delete(id);
+  }
+
+  addNote() {
+    NoteActions.create({task: 'New task'});
+  }
+
+  editNote(id, task) {
     // Don't modify if trying to set an empty value
     if(!task.trim()) {
       return;
     }
 
-    const notes = this.state.notes.map(note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
+    NoteActions.update({id, task});
+  }
 
-      return note;
-    });
-
-    this.setState({notes});
-  }; 
+ 
 }
