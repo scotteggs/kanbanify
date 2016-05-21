2eff3725522a7b4c3d389876b12689f1396ea49c
scotteggs <scotteggs@gmail.com>
2016-05-21 19:39:40 -0400

    enables basic drag and drop

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,25 +1,39 @@
 import React from 'react';
 
-import {DragSource} from 'react-dnd';
+import {DragSource, DropTarget} from 'react-dnd';
 import ItemTypes from '../constants/itemTypes';
 
 const noteSource = {
   beginDrag(props) {
-    console.log('begin dragging note', props);
+    return {
+      id: props.id
+    };
+  }
+};
 
-    return {};
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const targetId = targetProps.id;
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+    if(sourceId !== targetId) {
+      targetProps.onMove({sourceId,targetId});
+    }
   }
 };
 
 @DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
   connectDragSource: connect.dragSource()
 }))
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect) => ({
+  connectDropTarget: connect.dropTarget()
+}))
 export default class Note extends React.Component {
   render() {
-    const {connectDragSource, id, onMove, ...props} = this.props;
+    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props;
 
-    return connectDragSource(
+    return connectDragSource(connectDropTarget(
       <li {...props}>{props.children}</li>
-    );
+    ));
   }
 }
--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Editable from './Editable.jsx';
 import Note from './Note.jsx';
+import LaneActions from '../actions/LaneActions.js';
 
 export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map(note =>
-      <Note className="note" id={note.id} key={note.id}>
+      <Note className="note" id={note.id} key={note.id}
+          onMove={LaneActions.move}>
         <Editable
           editing={note.editing}
           value={note.task}
