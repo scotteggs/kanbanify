4b0060f61a5d97a240aca77866429839d8f60427
scotteggs <scotteggs@gmail.com>
2016-05-22 14:11:18 -0400

    enables webpack stats and configures deployment to github pages

--- webpack.config.js
+++ webpack.config.js
@@ -104,7 +104,7 @@ if(TARGET === 'start' || !TARGET) {
   });
 }
 
-if(TARGET === 'build') {
+if(TARGET === 'build' || TARGET === 'stats') {
   module.exports = merge(common, {
     entry: {
       vendor: Object.keys(pkg.dependencies).filter(function(v) {
